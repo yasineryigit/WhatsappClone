b6336c14fb79a4de3480198d06cce436e54bf44f
sancaryum <sancaryum@gmail.com>
2021-02-01 20:22:54 +0300

    Initial Commit & Splash Screen Activity Added

---
+++ app/src/main/java/com/ossovita/whatsappclone/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ossovita.whatsappclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
