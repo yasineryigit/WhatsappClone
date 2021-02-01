e5725fe0b99cf124e1e4e22561aea2d148803e4c
sancaryum <sancaryum@gmail.com>
2021-02-01 21:44:04 +0300

    Tabs added

--- app/src/main/java/com/ossovita/whatsappclone/MainActivity.java
+++ app/src/main/java/com/ossovita/whatsappclone/MainActivity.java
@@ -1,14 +1,70 @@
 package com.ossovita.whatsappclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.ossovita.whatsappclone.databinding.ActivityMainBinding;
+import com.ossovita.whatsappclone.menu.CallsFragment;
+import com.ossovita.whatsappclone.menu.ChatsFragment;
+import com.ossovita.whatsappclone.menu.StatusFragment;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        setUpWithViewPager(binding.viewPager);
+        binding.tabLayout.setupWithViewPager(binding.viewPager);
+
+    }
+
+    private void setUpWithViewPager(ViewPager viewPager){
+        MainActivity.SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new ChatsFragment(),"Chats");
+        adapter.addFragment(new StatusFragment(),"Status");
+        adapter.addFragment(new CallsFragment(),"Calls");
+        viewPager.setAdapter(adapter);
+    }
+
+    private static class SectionsPagerAdapter extends FragmentPagerAdapter{
+
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public SectionsPagerAdapter(FragmentManager manager) {super(manager);}
+
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title){
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 }
